74c7184cebe15333a9cb5164d304f6e41414a6d7
Caio Santos <78771496+CaioSan7os@users.noreply.github.com>
2022-11-24 01:16:56 +0000

    fizemos a consulta a banco, mas a parte de inserir no banco com algum probleminha

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 #Importa nosso arquivo Pessoa.py no diretório model
 from model.Pessoa import Pessoa
-
+from database.Database import Database
+from dao.PessoaDAO import PessoaDAO
 #Exemplo de uso
-poyatos = Pessoa(1, "Henrique Poyatos")
+poyatos = Pessoa(1, "Caio Santos")
 print(poyatos)
 
 #Quero mostrar só o nome
-print(poyatos.nome)`
+print(poyatos.nome)
+
+#chama o objeto de banco de dados
+db = Database()
+pessoaDAO = PessoaDAO(db.conexao, db.cursor)
+pessoas = pessoaDAO.getAll()
+for pessoa in pessoas:
+  print(pessoa)
